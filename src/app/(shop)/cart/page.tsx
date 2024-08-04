b4bea7423c0b84e62c2cfcc7bbd748e5b4d43ba1
Luis De La Cruz <ldelacruzg97@gmail.com>
2024-08-04 13:36:52 -0500

    feat: add footer

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -11,7 +11,7 @@ export default function CartPage() {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col items-center">
       <div className="flex flex-col w-full sm:w-[640px] lg:w-[1024px]">
         <Title title="CART" subtitle="Products to buy" />
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 mb-10">
---
+++ src/components/ui/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="flex justify-center gap-4 py-10 font-semibold text-xs text-gray-500">
+      <p>Teslo © 2024</p>
+      <p>Privacidad y legal</p>
+      <p>Localización de tiendas</p>
+    </footer >
+  )
+}
