257c084821f8278e198fecb3ad023adc5c51aa32
Luis De La Cruz <ldelacruzg97@gmail.com>
2024-08-12 12:06:13 -0500

    style: change loader page

--- src/app/(shop)/loading.tsx
+++ src/app/(shop)/loading.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { Loader } from "@/components";
+
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen font-bold text-4xl">
-      <p>Loading...</p>
+    <div className="fixed top-0 left-0 flex items-center justify-center w-screen h-screen">
+      <div className="fixed top-0 left-0 bg-white w-screen h-screen" />
+      <Loader />
     </div>
   );
 }
